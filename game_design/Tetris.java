f13b3d3bd10159f7c3f933b45703f3c69b0323cf
Jennifer <jenniferarai@hotmail.com>
2015-04-11 01:54:42 -0400

    fixed formatting in tetris

--- game_design/Tetris.java
+++ game_design/Tetris.java
@@ -1,37 +1,37 @@
 package game_design;
-	import java.awt.BorderLayout;
+import java.awt.BorderLayout;
 
-	import javax.swing.JFrame;
-	import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-	public class Tetris extends JFrame {
+public class Tetris extends JFrame {
 
-	    JLabel statusbar;
+	JLabel statusbar;
 
 
-	    public Tetris() {
+	public Tetris() {
 
-	        statusbar = new JLabel(" 0");
-	        add(statusbar, BorderLayout.SOUTH);
-	        Board board = new Board(this);
-	        add(board);
-	        board.start();
+		statusbar = new JLabel(" 0");
+		add(statusbar, BorderLayout.SOUTH);
+		Board board = new Board(this);
+		add(board);
+		board.start();
 
-	        setSize(200, 400);
-	        setTitle("Tetris");
-	        setDefaultCloseOperation(EXIT_ON_CLOSE);
-	   }
-
-	   public JLabel getStatusBar() {
-	       return statusbar;
-	   }
-
-	    public static void main(String[] args) {
-
-	        Tetris game = new Tetris();
-	        game.setLocationRelativeTo(null);
-	        game.setVisible(true);
-
-	    } 
+		setSize(200, 400);
+		setTitle("Tetris");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
+	public JLabel getStatusBar() {
+		return statusbar;
+	}
+
+	public static void main(String[] args) {
+
+		Tetris game = new Tetris();
+		game.setLocationRelativeTo(null);
+		game.setVisible(true);
+
+	} 
+}
 }
